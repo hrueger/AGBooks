11faf6f565eafe4357c895c44932f5a747e5d76e
Hannes Rüger <hannesrueger@gmx.de>
2019-08-06 12:48:10 +0200

    added instructions on step 7, stepper

--- src/app/step7/step7.component.ts
+++ src/app/step7/step7.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NavbarService } from "../services/navbar.service";
+import Stepper from 'bs-stepper';
 
 @Component({
   selector: "app-step7",
@@ -7,9 +8,16 @@ import { NavbarService } from "../services/navbar.service";
   styleUrls: ["./step7.component.scss"]
 })
 export class Step7Component implements OnInit {
-  constructor(private NavbarService: NavbarService) {}
-
+  private stepper: Stepper;
+  constructor(private NavbarService: NavbarService) { }
+  next() {
+    this.stepper.next();
+  }
   ngOnInit() {
     this.NavbarService.setStep(7);
+    this.stepper = new Stepper(document.querySelector('#stepper1'), {
+      linear: false,
+      animation: true
+    })
   }
 }
