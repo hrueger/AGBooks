e0ac355e044b2f5b5d2fd7526d58741caac7a86c
Hannes Rüger <hannesrueger@gmx.de>
2019-10-01 17:36:11 +0200

    updated all packages to latest version

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,6 +60,6 @@ import "zone.js/dist/zone"; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import "core-js/es6/reflect";
-import "core-js/es7/reflect";
+// import "core-js/es6/reflect";
+// import "core-js/es7/reflect";
 import "zone.js/dist/zone";
