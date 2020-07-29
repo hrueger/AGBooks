7b61f6c7844986567bb3884a96efe9e2277966c2
Hannes Rüger <hannesrueger@gmx.de>
2020-07-29 12:49:51 +0200

    (api) fixed check for admin middleware

--- api/src/middlewares/checkForAdmin.ts
+++ api/src/middlewares/checkForAdmin.ts
@@ -3,22 +3,25 @@ import { getRepository } from "typeorm";
 
 import { Admin } from "../entity/Admin";
 
-export const checkForAdmin = () => async (
-    req: Request, res: Response, next: NextFunction,
-): Promise<void> => {
+export const checkForAdmin = async (req: Request,
+    res: Response, next: NextFunction): Promise<void> => {
     const id = res.locals.jwtPayload.userId;
+
+    if (!res.locals.jwtPayload.isAdmin) {
+        res.status(401).send({ message: "Diese Aktion ist Ihnen nicht erlaubt!" });
+    }
 
     const userRepository = getRepository(Admin);
     let user: Admin;
     try {
         user = await userRepository.findOneOrFail(id);
     } catch {
-        res.status(401).send({ message: "errors.userNotFound", logout: true });
+        res.status(401).send({ message: "Der Benutzer wurde nicht gefunden!", logout: true });
     }
 
     if (user) {
         next();
     } else {
-        res.status(401).send({ message: "errors.notAllowed" });
+        res.status(401).send({ message: "Diese Aktion ist Ihnen nicht erlaubt!" });
     }
 };
