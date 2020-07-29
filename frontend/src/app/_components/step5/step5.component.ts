52b7d0ad0e2810dd7c99b1c5d91d5d401f1b6283
Hannes Rüger <hannesrueger@gmx.de>
2020-07-29 16:00:52 +0200

    (frontend) fixed queue

--- frontend/src/app/_components/step5/step5.component.ts
+++ frontend/src/app/_components/step5/step5.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 import { Router } from "@angular/router";
 import { NavbarService } from "../../_services/navbar.service";
 import { RemoteService } from "../../_services/remote.service";
@@ -13,24 +13,36 @@ import { getApiUrl } from "../../_utils/utils";
 export class Step5Component implements OnInit {
     public ordersLeft: any = "...";
     public orderReady = false;
+    public error: any;
 
     constructor(
         private navbarService: NavbarService,
         private remoteService: RemoteService,
         private router: Router,
         private authService: AuthenticationService,
+        private cdr: ChangeDetectorRef,
     ) { }
 
     public ngOnInit(): void {
         this.navbarService.setStep(5);
         const { token } = this.authService.currentUserValue;
-        const source = new EventSource(`${getApiUrl()}?queue&token=${token}`);
-        source.addEventListener("update", (message: any) => {
-            const data = JSON.parse(message.data);
+        const source = new EventSource(`${getApiUrl()}order/live?authorization=${token}`);
+        source.onmessage = (m) => {
+            const data = JSON.parse(m.data);
             this.ordersLeft = data.ordersLeft;
             this.orderReady = data.orderReady;
-        });
+            this.cdr.detectChanges();
+        };
+        source.onerror = (e) => {
+            this.error = e;
+            this.cdr.detectChanges();
+            setTimeout(() => {
+                // eslint-disable-next-line no-restricted-globals
+                location.reload();
+            }, 5000);
+        };
     }
+
     public previous(): void {
         this.router.navigate(["step", "4"]);
     }
