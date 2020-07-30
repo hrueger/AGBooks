a36bc0ca71741a5764e4d42534058e8817a31bcf
Hannes Rüger <hannesrueger@gmx.de>
2020-07-30 16:27:46 +0200

    (backend) (mobile) fixed loading when not loggedin

--- backend/src/app/app.component.tns.ts
+++ backend/src/app/app.component.tns.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
 import { first } from "rxjs/operators";
 import { Order } from "./_models/Order";
 import { RemoteService } from "./_services/remote.service";
+import { AuthenticationService } from "./_services/authentication.service";
 
 @Component({
     selector: "app",
@@ -10,52 +11,58 @@ import { RemoteService } from "./_services/remote.service";
 })
 
 export class AppComponent {
-  public orders: Order[];
-  public doneOrders: Order[];
-  public acceptedOrders: Order[];
-  @ViewChild("rsd", { static: false }) public rSideDrawer: ElementRef;
-  constructor(private remoteService: RemoteService) { }
+    public orders: Order[];
+    public doneOrders: Order[];
+    public acceptedOrders: Order[];
+    @ViewChild("rsd", { static: false }) public rSideDrawer: ElementRef;
+    constructor(
+        private remoteService: RemoteService,
+        private authenticationService: AuthenticationService,
+    ) { }
 
-  public ngOnInit(): void {
-      this.remoteService
-          .getOrders()
-          .pipe(first())
-          .subscribe(
-              (orders) => {
-                  if (orders) {
-                      this.sortOrders(orders);
-                  }
+    public ngOnInit(): void {
+        if (this.authenticationService.currentUserValue) {
+            this.remoteService
+                .get("order/all")
+                .pipe(first())
+                .subscribe(
+                    (orders) => {
+                        if (orders) {
+                            this.sortOrders(orders);
+                        }
+                    },
+                );
+        }
+        setInterval(() => {
+            if (this.authenticationService.currentUserValue) {
+                this.remoteService
+                    .get("order/all")
+                    .pipe(first())
+                    .subscribe(
+                        (o) => {
+                            if (o) {
+                                this.sortOrders(o);
+                            }
+                        },
+                    );
+            }
+        }, 30000);
+    }
+    public sortOrders(orders: Order[]): void {
+        if (orders.length) {
+            this.orders = orders.filter(
+                (order: any) => order.done == "0" && order.accepted == "0",
+            );
+            this.doneOrders = orders.filter(
+                (order: any) => order.done == "1" && order.accepted == "0",
+            );
+            this.acceptedOrders = orders.filter(
+                (order: any) => order.done == "1" && order.accepted == "1",
+            );
+        }
+    }
 
-                  setInterval(() => {
-                      this.remoteService
-                          .getOrders()
-                          .pipe(first())
-                          .subscribe(
-                              (o) => {
-                                  if (o) {
-                                      this.sortOrders(o);
-                                  }
-                              },
-                          );
-                  }, 30000);
-              },
-          );
-  }
-  public sortOrders(orders: Order[]): void {
-      if (orders.length) {
-          this.orders = orders.filter(
-              (order: any) => order.done == "0" && order.accepted == "0",
-          );
-          this.doneOrders = orders.filter(
-              (order: any) => order.done == "1" && order.accepted == "0",
-          );
-          this.acceptedOrders = orders.filter(
-              (order: any) => order.done == "1" && order.accepted == "1",
-          );
-      }
-  }
-
-  public hideDrawer(): void {
-      this.rSideDrawer.nativeElement.toggleDrawerState();
-  }
+    public hideDrawer(): void {
+        this.rSideDrawer.nativeElement.toggleDrawerState();
+    }
 }
