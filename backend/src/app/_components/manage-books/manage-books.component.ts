f19e50497d9c33252ecf4363d0e4e3ea792239da
H. Rüger <hannesrueger@gmx.de>
2021-08-29 10:01:57 +0200

    (backend) create new books

--- backend/src/app/_components/manage-books/manage-books.component.ts
+++ backend/src/app/_components/manage-books/manage-books.component.ts
@@ -3,59 +3,66 @@ import { Book } from 'src/app/_models/Book';
 import { RemoteService } from 'src/app/_services/remote.service';
 
 @Component({
-  selector: 'app-manage-books',
-  templateUrl: './manage-books.component.html',
-  styleUrls: ['./manage-books.component.scss']
+    selector: 'app-manage-books',
+    templateUrl: './manage-books.component.html',
+    styleUrls: ['./manage-books.component.scss']
 })
 export class ManageBooksComponent implements OnInit {
-  public books: Book[] = [];
-  public apiUrl = "/api/";
-  public subjects: string[] = [
-    "Biologie",
-    "Chemie",
-    "Deutsch",
-    "Englisch",
-    "Ethik",
-    "Französisch",
-    "Geographie",
-    "Geschichte",
-    "Informatik",
-    "Latein",
-    "Mathematik",
-    "Musik",
-    "Physik",
-    "Religion ev",
-    "Religion rk",
-    "Sozialkunde",
-    "Spanisch",
-    "Sport",
-    "W&R"
-  ];
-  constructor(private remoteService: RemoteService) { }
+    public books: Book[] = [];
+    public apiUrl = "/api/";
+    public loading = true;
+    public subjects: string[] = [
+        "Biologie",
+        "Chemie",
+        "Deutsch",
+        "Englisch",
+        "Ethik",
+        "Französisch",
+        "Geographie",
+        "Geschichte",
+        "Informatik",
+        "Latein",
+        "Mathematik",
+        "Musik",
+        "Physik",
+        "Religion ev",
+        "Religion rk",
+        "Sozialkunde",
+        "Spanisch",
+        "Sport",
+        "W&R"
+    ];
+    constructor(private remoteService: RemoteService) { }
 
-  public ngOnInit(): void {
-      this.remoteService.get("books/admin").subscribe((books: Book[]) => {
-          this.books = books.map((b) => {
-            for (const key of ["5", "6", "7", "8", "9", "10", "Q11", "Q12", "uebergang"]) {
-              b[key] = b[key] == "1";
-            }
-            return b;
-          });
-      });
-  }
+    public ngOnInit(): void {
+        this.loading = true;
+        this.remoteService.get("books/admin").subscribe((books: Book[]) => {
+            this.gotBooks(books);
+        });
+    }
 
-  public newBook(): void {
-      //
-  }
+    private gotBooks(books: Book[]) {
+        this.loading = false;
+        this.books = books;
+    }
 
-  public removeBook(book: Book): void {
-      // eslint-disable-next-line
-      if (!confirm("Soll dieses Buch wirklich gelöscht werden?")) {
-          return;
-      }
-      this.remoteService.delete(`auth/books/${book.id}`).subscribe(() => {
-          this.ngOnInit();
-      });
-  }
+    public newBook(): void {
+        this.loading = true;
+        this.remoteService.post(`books/admin`, {}).subscribe((books: Book[]) => {
+            this.gotBooks(books);
+            alert("Das Buch wurde erstellt. Es befindet sich wahrscheinlich ganz unten auf der Liste.")
+        });
+    }
+
+    public removeBook(book: Book): void {
+        // eslint-disable-next-line
+        if (!confirm("Soll dieses Buch wirklich gelöscht werden?")) {
+            return;
+        }
+        this.loading = true;
+        this.remoteService.delete(`auth/books/${book.id}`).subscribe(() => {
+            this.ngOnInit();
+        });
+    }
 
 }
