74203144e0cb501dbcd81e9ddec6e1f01ac4e75c
H. Rüger <hannesrueger@gmx.de>
2021-08-30 13:32:54 +0200

    (api) fix csp

--- api/src/index.ts
+++ api/src/index.ts
@@ -76,7 +76,9 @@ createConnection({
         // This sets up secure rules for CORS, see https://developer.mozilla.org/de/docs/Web/HTTP/CORS
         app.use(cors());
         // This secures the app with some http headers
-        app.use(helmet() as any);
+        app.use(helmet({
+            contentSecurityPolicy: false,
+        }) as any);
         // This transforms the incoming JSON body into objects
         app.use(express.json() as any);
 
