e107b39e8dfb9e2d4e6b17ead628e5a8916f0437
H. Rüger <hannesrueger@gmx.de>
2022-09-05 14:04:03 +0200

    use @vercel/ncc instead of @zeit/ncc

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ npm install
 npm run build -- --output-path=../docker/backend
 cd ../api
 npm install
-npx @zeit/ncc build src/index.ts -o ../docker/api
+npx @vercel/ncc build src/index.ts -o ../docker/api
 cp -r assets ../docker/api/
 cd ../docker
 PACKAGE_VERSION=$(cat ../frontend/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
